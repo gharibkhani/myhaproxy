9a9580b3a6ec873de16e937bd5562ddb0a65d8d7
gharibkhani <gharibkhani@gmail.com>
2017-11-30 14:59:50 -0600

    Initial Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [
+	{
+		'hostname' => 'ec2-34-204-79-163.compute-1.amazonaws.com',
+		'ipaddress' => '34.204.79.163',
+		'port' => 80,
+		'ssl_port' => 80
+	}
+]
+
+include_recipe 'haproxy::manual'
