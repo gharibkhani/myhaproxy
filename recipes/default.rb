1234a188ebacf0552eea9e4fbd0a03f806c2dcae
gharibkhani <gharibkhani@gmail.com>
2017-12-01 10:12:48 -0600

    Version 2

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,21 +1,13 @@
-#
-# Cookbook:: myhaproxy
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
-
-node.default['haproxy']['members'] = [
-	{
-		'hostname' => 'ec2-34-204-79-163.compute-1.amazonaws.com',
-		'ipaddress' => '34.204.79.163',
-		'port' => 80,
-		'ssl_port' => 80
-	},{
-		'hostname' => 'ec2-52-201-237-29.compute-1.amazonaws.com',
-		'ipaddress' => '52.201.237.29',
-		'port' => 80,
-		'ssl_port' => 80
-	}
-]
-
+all_web_nodes = search('node','role:web_server')
+members = []
+all_web_nodes.each do |web_node|
+member = {
+'hostname' => web_node['cloud']['public_hostname'],
+'ipaddress' => web_node['cloud']['public_ipv4'],
+'port' => 80,
+'ssl_port' => 80
+}
+members.push(member)
+end
+node.default['haproxy']['members'] = members
 include_recipe 'haproxy::manual'
